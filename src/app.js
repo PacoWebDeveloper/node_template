b5221996021538c84c847b716df83a440da2b9c2
Francisco Ortiz <pacoortiz16@hotmail.com>
2023-02-24 20:53:37 -0600

    Adding libraries needed

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const config = require('../config')
+const { host, port } = config
 
 const app = express()
 
 app.use(express.json())
 
-app.listen(9000, () => {
-    console.log(`Server running on port: 9000`)
+app.listen(port, () => {
+    console.log(`Server running on: ${host}:${port}`)
 })
